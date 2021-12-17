be227cab6244422d3e77b9e4eae65cd96863640f
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2021-12-17 07:46:24 +0600

    feat(assets): resized image to reduce size

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,2 +1,2 @@
-export { default as background_1 } from './background1.png';
-export { default as product_1 } from './product1.jpg';
+export { default as background_1 } from '../../../assets/background1.png';
+export { default as product_1 } from '../../../assets/product1.jpg';
