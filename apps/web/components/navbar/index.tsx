cf93a9f318185b4d3669ba8e496d8ec7f4d9e654
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-01-17 12:02:16 +0600

    feat(styling): add styles from navbar

--- apps/web/components/navbar/index.tsx
+++ apps/web/components/navbar/index.tsx
@@ -1,25 +1,66 @@
 import React from "react";
+import {BsCartFill} from "react-icons/bs";
+import {FaBars, FaTimes, FaUser} from "react-icons/fa";
 
 export const Navbar: React.FC = () => {
+  const [loggedIn, setLoggedIn] = React.useState<boolean>(true);
+  const [showMenu, setShowMenu] = React.useState<boolean>(false);
+
+  const toggleNavbarMenu = (e: any, val?: boolean): void => {
+    if (val) {
+      setShowMenu(val);
+    } else {
+      setShowMenu((prev) => !prev);
+    }
+  };
+
   return (
     <nav className="navbar">
-      <h1 className="logo">FS</h1>
-      <div className="navbar-menu">
-        <ul className="navbar-links">
-          <li className="navbar-links-item">Home</li>
-          <li className="navbar-links-item">Home</li>
-          <li className="navbar-links-item">Home</li>
-          <li className="navbar-links-item">
-            <button className="btn--primary">Login</button>
-          </li>
-          <li className="navbar-links-item">
-            <button className="btn--secondary">Sign Up</button>
-          </li>
-          <li className="navbar-links-item">
-            <button className="icon-btn--primary">C</button>
-          </li>
-        </ul>
-      </div>
+      <a href="/">
+        <span className="logo">FS</span>
+      </a>
+      <div style={{flexGrow: 1}} />
+      <ul className={`navbar-menu ${showMenu ? "active" : "inactive"}`}>
+        <li className="navbar-menu-item">
+          <a href="/">Home</a>
+        </li>
+        <li className="navbar-menu-item">
+          <a href="/">Categories</a>
+        </li>
+        <li className="navbar-menu-item">
+          <a href="/">Tech</a>
+        </li>
+        <li className="navbar-menu-item">
+          <a href="/">Accessories</a>
+        </li>
+        <li className="navbar-menu-item">
+          <a href="/">About</a>
+        </li>
+        <li className="navbar-menu-item">
+          <div className="user-panel">
+            {loggedIn ? (
+              <button className="icon-btn--primary">
+                <FaUser />
+              </button>
+            ) : (
+              <>
+                <button className="btn--secondary">Sign Up</button>
+                <button className="btn--primary">Login</button>
+              </>
+            )}
+          </div>
+        </li>
+      </ul>
+      <button
+        id="menu-toggle-btn"
+        className="icon-btn--secondary"
+        onClick={toggleNavbarMenu}
+      >
+        {showMenu ? <FaTimes /> : <FaBars />}
+      </button>
+      <button className="icon-btn--primary">
+        <BsCartFill />
+      </button>
     </nav>
   );
 };
