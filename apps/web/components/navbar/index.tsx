d37b715da4122b8ea4b8b808244f3560bb9c5f68
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-01-20 16:27:28 +0600

    feat(navbar): update navbar style
    BREAKING CHANGE: rebuild categories button. now hovering to the
    categories menu will show another sub menu in desktop mode. while in
    mobile mode the submenu will be visible under the categories

--- apps/web/components/navbar/index.tsx
+++ apps/web/components/navbar/index.tsx
@@ -6,14 +6,23 @@ import Link from "next/link";
 export const Navbar: React.FC = () => {
   const [loggedIn, setLoggedIn] = React.useState<boolean>(false);
   const [showMenu, setShowMenu] = React.useState<boolean>(false);
+  const [showHoverMenu, setShowHoverMenu] = React.useState(false);
 
-  const toggleNavbarMenu = (e: any, val?: boolean): void => {
+  function toggleNavbarMenu(e: any, val?: boolean) {
     if (val) {
       setShowMenu(val);
     } else {
       setShowMenu((prev) => !prev);
     }
-  };
+  }
+
+  function toggleHoverMenu(e: any, hover: boolean) {
+    if (hover) {
+      setShowHoverMenu(true);
+    } else {
+      setShowHoverMenu(false);
+    }
+  }
 
   return (
     <nav className="navbar">
@@ -23,19 +32,40 @@ export const Navbar: React.FC = () => {
       <div style={{flexGrow: 1}} />
       <ul className={`navbar-menu ${showMenu ? "active" : "inactive"}`}>
         <li className="navbar-menu-item">
-          <a href="/">Home</a>
+          <Link href="/">Home</Link>
         </li>
         <li className="navbar-menu-item">
-          <a href="/">Categories</a>
+          <Link href="/about">About</Link>
         </li>
         <li className="navbar-menu-item">
-          <a href="/">Tech</a>
-        </li>
-        <li className="navbar-menu-item">
-          <a href="/">Accessories</a>
-        </li>
-        <li className="navbar-menu-item">
-          <a href="/">About</a>
+          <div
+            className={`hover-menu ${showHoverMenu ? "active" : "inactive"}`}
+            onMouseMove={(e) => toggleHoverMenu(e, true)}
+            onMouseLeave={(e) => toggleHoverMenu(e, false)}
+          >
+            <button
+              className={`hover-menu-btn ${
+                showHoverMenu ? "active" : "inactive"
+              }`}
+            >
+              Categories
+            </button>
+            <ul
+              className={`hover-menu-list ${
+                showHoverMenu ? "active" : "inactive"
+              }`}
+            >
+              <li className="hover-menu-item">
+                <Link href="/categories/clothes">Clothes</Link>
+              </li>
+              <li className="hover-menu-item">
+                <Link href="/categories/technologies">Technologies</Link>
+              </li>
+              <li className="hover-menu-item">
+                <Link href="/categories/accessories">Accessories</Link>
+              </li>
+            </ul>
+          </div>
         </li>
         <li className="navbar-menu-item">
           <div className="user-panel">
